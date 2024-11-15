a385f01e897e44ff08717fe4d8b716734f6e8459
padak <diseor1701@naver.com>
2024-11-15 20:06:18 +0900

    Client change

--- ACT-Project_DX11/Client/Client.cpp
+++ ACT-Project_DX11/Client/Client.cpp
@@ -216,7 +216,7 @@ void Client::Init()
 		player->GetOrAddTransform()->SetScale(Vec3(0.01f));
 
 		shared_ptr<Model> playerModel = make_shared<Model>();
-		// Model
+		// Player::Model
 		{
 			playerModel->ReadModel(L"Player/Player");
 			playerModel->ReadMaterial(L"Player/Player");
@@ -228,10 +228,10 @@ void Client::Init()
 			//playerModel->ReadAnimation(L"Player/Crab_Atk_Combo2", AnimationState::Attack2);
 			//playerModel->ReadAnimation(L"Player/Crab_Atk_Combo3", AnimationState::Attack3);
 			//playerModel->ReadAnimation(L"Player/Crab_Atk_Combo4", AnimationState::Attack4);
-			
+
 			//playerModel->ReadAnimation(L"Player/Crab_Death");
 			//playerModel->ReadAnimation(L"Player/Crab_GetUp");
-			
+
 			// Weapon
 			shared_ptr<Model> weaponModel = make_shared<Model>();
 			{
@@ -241,9 +241,10 @@ void Client::Init()
 			}
 
 			ModelMesh& weaponMesh = *weaponModel->GetMeshes()[0];
-			// ���� �� ���� �� ���� ���̱�
 			playerModel->AddDummyBoneAndAttach(weaponMesh, L"Hand_Grip_L", L"WeaponDummy");
 		}
+
+		// Player::ModelAnimator
 		shared_ptr<ModelAnimator> ma1 = make_shared<ModelAnimator>(renderShader);
 		player->AddComponent(ma1);
 		{
@@ -251,138 +252,15 @@ void Client::Init()
 			player->GetModelAnimator()->SetPass(2);
 		}
 
-		// Weapon (Fork)
-		{
-			auto weapon = make_shared<GameObject>();
-			weapon->GetOrAddTransform()->SetPosition(Vec3(0.f));
-			weapon->GetOrAddTransform()->SetScale(Vec3(1.00f));
-
-			// CustomData -> Memory
-			shared_ptr<class Model> weaponModel = make_shared<Model>();
-			weaponModel->ReadModel(L"Fork/Fork");
-			weaponModel->ReadMaterial(L"Fork/Fork");
-
-			weapon->AddComponent(make_shared<ModelRenderer>(renderShader));
-			{
-				weapon->GetModelRenderer()->SetModel(weaponModel);
-				weapon->GetModelRenderer()->SetPass(1);
-			}
-
-			shared_ptr<ModelBone> handBone = playerModel->GetBoneByName(L"Hand_Grip_L");
-
-			CUR_SCENE->Add(weapon);
-		}
-
-		// PlayerScript
+		// Player::PlayerScript
 		shared_ptr<PlayerScript> playerScript = make_shared<PlayerScript>();
 		playerScript->SetPlayer(playerModel);
 		playerScript->SetModelAnimator(ma1);
-
-		// Debug Test
-		playerScript->_look = lookCube;
-		playerScript->_up = upCube;
-		playerScript->_right = rightCube;
 
 		player->AddComponent(playerScript);
 
 		CUR_SCENE->Add(player);
 		CUR_SCENE->SetPlayer(player);
-	}
-
-	// Debug Object
-	{
-		auto transform = player->GetTransform();
-
-		auto mesh = RESOURCES->Get<Mesh>(L"Cube");
-		// Look ���� ť��
-		{
-			// Look ���� ǥ�ÿ� ������ ����
-			auto material = make_shared<Material>();
-			material->SetShader(renderShader);
-			MaterialDesc& desc = material->GetMaterialDesc();
-			desc.ambient = Vec4(1.f);
-			desc.diffuse = Vec4(1.0f, 0.0f, 0.0f, 1.0f); // ������
-			desc.specular = Vec4(1.f);
-
-			lookCube->GetOrAddTransform()->SetPosition(transform->GetPosition() + transform->GetLook() * 2.5f);
-			lookCube->GetOrAddTransform()->SetScale(Vec3(0.1f, 0.1f, 5.0f));  // Z �������� �����ϰ�
-			lookCube->AddComponent(make_shared<MeshRenderer>());
-			{
-				lookCube->GetMeshRenderer()->SetMaterial(material);
-				lookCube->GetMeshRenderer()->SetMesh(mesh);
-				lookCube->GetMeshRenderer()->SetPass(0);
-			}
-			CUR_SCENE->Add(lookCube);
-		}
-
-		// Up ���� ť��
-		{
-			// Up ���� ǥ�ÿ� �ʷϻ� ����
-			auto material = make_shared<Material>();
-			material->SetShader(renderShader);
-			MaterialDesc& desc = material->GetMaterialDesc();
-			desc.ambient = Vec4(1.f);
-			desc.diffuse = Vec4(0.0f, 1.0f, 0.0f, 1.0f);  // �ʷϻ�
-			desc.specular = Vec4(1.f);
-			
-			upCube->GetOrAddTransform()->SetPosition(transform->GetPosition() + transform->GetUp() * 2.5f);
-			upCube->GetOrAddTransform()->SetScale(Vec3(0.1f, 5.0f, 0.1f));  // Y �������� �����ϰ�
-			upCube->AddComponent(make_shared<MeshRenderer>());
-			{
-				upCube->GetMeshRenderer()->SetMaterial(material);
-				upCube->GetMeshRenderer()->SetMesh(mesh);
-				upCube->GetMeshRenderer()->SetPass(0);
-			}
-
-			CUR_SCENE->Add(upCube);
-		}
-
-		// Right ���� ť��
-		{
-			// Right ���� ǥ�ÿ� �Ķ��� ����
-			auto material = make_shared<Material>();
-			material->SetShader(renderShader);
-			MaterialDesc& desc = material->GetMaterialDesc();
-			desc.ambient = Vec4(1.f);
-			desc.diffuse = Vec4(0.0f, 0.0f, 1.0f, 1.0f);  // �Ķ���
-			desc.specular = Vec4(1.f);
-
-			rightCube->GetOrAddTransform()->SetPosition(transform->GetPosition() + transform->GetRight() * 2.5f);
-			rightCube->GetOrAddTransform()->SetScale(Vec3(5.0f, 0.1f, 0.1f));  // X �������� �����ϰ�
-			rightCube->AddComponent(make_shared<MeshRenderer>());
-			{
-				rightCube->GetMeshRenderer()->SetMaterial(material);
-				rightCube->GetMeshRenderer()->SetMesh(mesh);
-				rightCube->GetMeshRenderer()->SetPass(0);
-			}
-		
-			CUR_SCENE->Add(rightCube);
-		}
-	}
-	// Enemy
-	auto enemy = make_shared<GameObject>();
-
-	{
-		enemy->GetOrAddTransform()->SetPosition(Vec3(10, 0, 10));
-		enemy->GetOrAddTransform()->SetLocalRotation(Vec3(0, XMConvertToRadians(180), 0)); // XMConvertToRadians()
-		enemy->GetOrAddTransform()->SetScale(Vec3(0.0001f));
-
-		shared_ptr<Model> enemyModel = make_shared<Model>();
-		// Model
-		{
-			enemyModel->ReadModel(L"Enemy/Rangoon");
-			enemyModel->ReadMaterial(L"Enemy/Rangoon");
-
-			enemyModel->ReadAnimation(L"Enemy/Rangoon_idle", AnimationState::Idle);
-			enemyModel->ReadAnimation(L"Enemy/Rangoon_hit", AnimationState::Hit);
-		}
-		shared_ptr<ModelAnimator> ma2 = make_shared<ModelAnimator>(renderShader);
-		enemy->AddComponent(ma2);
-		{
-			enemy->GetModelAnimator()->SetModel(enemyModel);
-			enemy->GetModelAnimator()->SetPass(2);
-		}
-		CUR_SCENE->Add(enemy);
 	}
 
 	// Terrain

211fde7cacb7d72afdbead68dfd4c903816819ca
kimjaehee <dkdlwpa212@gmail.com>
2024-12-03 14:12:48 +0900

    fix(KJH) : KJH_FixMapObj_52
    맵오브젝트 삭제 시 콜라이더 남아있고, 코드상 오류 수정.

--- ACT-Project_DX11/Engine/CollisionManager.h
+++ ACT-Project_DX11/Engine/CollisionManager.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <d3d11.h>
 
 class BaseCollider;
 class Rigidbody;
@@ -15,6 +16,8 @@ public:
 	void AddCollider(shared_ptr<BaseCollider> collider);
 	void AddRigidbody(shared_ptr<Rigidbody> rigidbody);
 	void ApplyForce(shared_ptr<Rigidbody> rigidbody, const Vec3& target, const Vec3& source, float massDifference);
+
+	void Remove(shared_ptr<GameObject> obj);
 
 private:
 	void HandleCollision(shared_ptr<BaseCollider> colliderA, shared_ptr<BaseCollider> colliderB);

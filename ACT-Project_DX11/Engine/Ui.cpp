4d137fd78009517a484bc67dcd67b5214ccc08f5
kimjaehee <dkdlwpa212@gmail.com>
2024-12-07 22:21:43 +0900

    07

--- ACT-Project_DX11/Engine/Ui.cpp
+++ ACT-Project_DX11/Engine/Ui.cpp
@@ -15,24 +15,15 @@ void Ui::PreUpdate()
     if (owner != nullptr)
     {
         Vec3 pos = owner->GetTransform()->GetLocalPosition();
-        pos += Vec3{ 1.5f, 3.0f, 0.0f };
-
-        Vec3 uiVeiwpos = XMVector3Transform(pos, Camera::S_MatView);
-        Vec3 uiClipPos = XMVector3Transform(uiVeiwpos,Camera::S_MatProjection);
-        // 투영행렬에서 밖에 있는 애는 알아서 컬링되게끔.
-
+        pos += Vec3{ 0, 3.0f, 0.0f };
+        Matrix VP = Camera::S_MatView * Camera::S_MatProjection;
+        Vec3 uiClipPos = XMVector3TransformCoord(pos, VP);
         float d = sqrt(uiClipPos.x * uiClipPos.x + uiClipPos.y * uiClipPos.y + uiClipPos.z * uiClipPos.z);
-
-
-
         float xNDC = uiClipPos.x / d;
         float yNDC = uiClipPos.y / d;
-
         float screenX = (xNDC + 1.0f) * 400.f - 400;
         float screenY = (yNDC + 1.0f) * 300.f - 300;
-
-        Vec3 screen = {screenX,screenY,0.0f};
-
+        Vec3 screen = {screenX,screenY,1.0f};
         GetGameObject()->GetTransform()->SetLocalPosition(screen);
     }
 }
